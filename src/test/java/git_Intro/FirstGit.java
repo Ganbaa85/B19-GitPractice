54cca3d00d90ad48dd7acd116f414e9fd0d8d6dd
Ganbaa <ganbjargal@gmail.com>
2024-03-17 10:50:34 -0500

    my second push

--- src/test/java/git_Intro/FirstGit.java
+++ src/test/java/git_Intro/FirstGit.java
@@ -3,5 +3,6 @@ package git_Intro;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("First Git");
+        System.out.println("My second git push");
     }
 }
