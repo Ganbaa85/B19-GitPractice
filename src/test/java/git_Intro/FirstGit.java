3fbd2f884b9d7d081d6f7aa604f8fbb82694bc7c
Ganbaa <ganbjargal@gmail.com>
2024-03-17 11:00:53 -0500

    myBranchMessage

--- src/test/java/git_Intro/FirstGit.java
+++ src/test/java/git_Intro/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("First Git");
         System.out.println("My second git push");
+        System.out.println("My branches push");
     }
 }
